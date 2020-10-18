d23c796c39ed950d90500fd7f025ca7dea4be701
Marcus Roberts <m.roberts@nittygritty.net>
2020-10-18 21:14:38 +0100

    Stopped using webpack and took the simpler bundling approach to add the server executable too

--- editor-extensions/vscode/client/src/extension.ts
+++ editor-extensions/vscode/client/src/extension.ts
@@ -1,3 +1,5 @@
+/* This file is largely copied from vscode's sample library.
+	 The original copyright notice is reproduced below. */
 /* --------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
