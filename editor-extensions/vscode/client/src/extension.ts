99c446c8ba53e8dbd446f48d3aa44aace4c03e88
Marcus Roberts <marcus@marcusr.com>
2022-05-14 21:21:44 +0100

    Handle multiple workspaces with multiple clients

--- editor-extensions/vscode/client/src/extension.ts
+++ editor-extensions/vscode/client/src/extension.ts
@@ -5,13 +5,15 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from "path";
 import {
   workspace,
   commands,
   ExtensionContext,
   languages,
   Disposable,
+  TextDocument,
+  WorkspaceFolder,
+  Uri,
 } from "vscode";
 
 import {
@@ -21,44 +23,131 @@ import {
   TransportKind,
 } from "vscode-languageclient";
 
+import * as path from "path";
+
+import * as fs from "fs";
+
 import { GrainDocCompletionProvider } from "./GrainDocCompletionProvider";
 
-let client: LanguageClient;
-
-let disposables: Disposable[] = [];
+let buildScriptPath = "script/grainfind.js";
+//let client: LanguageClient;
 
 let grainDocCompletionProvider: GrainDocCompletionProvider;
 
-let runArgs: string[] = ["lsp","--debuglsp"];
-let debugArgs: string[] = ["lsp","--debuglsp"];
+let runArgs: string[] = ["lsp"];
+let debugArgs: string[] = ["lsp", "--debuglsp"];
 
 let executablePath = "grain";
 
-let serverOptions: ServerOptions = {
-  run: {
-    command: executablePath,
-    transport: TransportKind.stdio,
-    args: runArgs,
-  },
-  debug: {
-    command: executablePath,
-    transport: TransportKind.stdio,
-    args: debugArgs,
-  },
-};
+const isWindows = /^win32/.test(process.platform);
 
-// Options to control the language client
-let clientOptions: LanguageClientOptions = {
-  // Register the server for grain documents
-  documentSelector: [{ scheme: "file", language: "grain" }],
-  synchronize: {
-    // Notify the server about file changes to '.clientrc files contained in the workspace
-    fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-  },
-};
+let defaultClient: LanguageClient;
+const clients: Map<string, LanguageClient> = new Map();
 
-const startClient = () => {
-  client = new LanguageClient(
+let _sortedWorkspaceFolders: string[] | undefined;
+function sortedWorkspaceFolders(): string[] {
+  if (_sortedWorkspaceFolders === void 0) {
+    _sortedWorkspaceFolders = workspace.workspaceFolders
+      ? workspace.workspaceFolders
+          .map((folder) => {
+            let result = folder.uri.toString();
+            if (result.charAt(result.length - 1) !== "/") {
+              result = result + "/";
+            }
+            return result;
+          })
+          .sort((a, b) => {
+            return a.length - b.length;
+          })
+      : [];
+  }
+  return _sortedWorkspaceFolders;
+}
+workspace.onDidChangeWorkspaceFolders(
+  () => (_sortedWorkspaceFolders = undefined)
+);
+
+function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
+  const sorted = sortedWorkspaceFolders();
+  for (const element of sorted) {
+    let uri = folder.uri.toString();
+    if (uri.charAt(uri.length - 1) !== "/") {
+      uri = uri + "/";
+    }
+    if (uri.startsWith(element)) {
+      return workspace.getWorkspaceFolder(Uri.parse(element))!;
+    }
+  }
+  return folder;
+}
+
+function filenameFromUri(uri: Uri) {
+  let filename = uri.fsPath;
+
+  // Packaged Grain doesn't understand lowercase drive letters.
+  // If authority is not empty, then we can skip since this is
+  // a UNC path.
+  if (isWindows && uri.authority === "") {
+    filename = filename[0].toUpperCase() + filename.substring(1);
+  }
+
+  return filename;
+}
+
+const startClient = (workspace_uri: Uri) => {
+  // Not sure if this can technically change between VSCode restarts. Even if it does,
+  // it is likely to be swapped with PowerShell, which understands the `.cmd` executables.
+  const needsCMD =
+    isWindows && process.env.ComSpec && /cmd.exe$/.test(process.env.ComSpec);
+
+  if (
+    needsCMD &&
+    !executablePath.endsWith(".cmd") &&
+    !executablePath.endsWith(".exe")
+  ) {
+    executablePath += ".cmd";
+  }
+
+  let run_path = executablePath;
+  let localRunArgs = [...runArgs];
+  let localDebugArgs = [...debugArgs];
+
+  if (workspace_uri) {
+    let workspaceFolder = filenameFromUri(workspace_uri);
+    const absoluteBuildScript = path.join(
+      workspaceFolder + "/" + buildScriptPath
+    );
+
+    if (fs.existsSync(absoluteBuildScript)) {
+      run_path = "node";
+      localRunArgs = [absoluteBuildScript, ...localRunArgs];
+      localDebugArgs = [absoluteBuildScript, ...localDebugArgs];
+    }
+  }
+  let serverOptions: ServerOptions = {
+    run: {
+      command: run_path,
+      transport: TransportKind.stdio,
+      args: localRunArgs,
+    },
+    debug: {
+      command: run_path,
+      transport: TransportKind.stdio,
+      args: localDebugArgs,
+    },
+  };
+
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for grain documents
+    documentSelector: [{ scheme: "file", language: "grain" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  const client = new LanguageClient(
     "grain_langage_server",
     "Grain Language Server",
     serverOptions,
@@ -74,37 +163,81 @@ const startClient = () => {
     );
   });
 
-  // Start the client. This will also launch the server
-  client.start();
+  return client;
 };
 
 const restart = () => {
-  console.log("restart");
-  if (client) {
-    client.stop().then(() => {
-      if (disposables) {
-        disposables.forEach((item) => item.dispose());
-      }
-      disposables = [];
-      client = undefined;
-      startClient();
-    });
-  } else startClient();
+  const promises: Thenable<void>[] = [];
+  if (defaultClient) {
+    promises.push(defaultClient.stop());
+  }
+  for (const client of clients.values()) {
+    promises.push(client.stop());
+  }
+
+  Promise.all(promises).then(() => {
+    if (defaultClient) {
+      defaultClient.start();
+    }
+    for (const client of clients.values()) {
+      client.start();
+    }
+  });
 };
 
 commands.registerCommand("grain_language_server.restart", restart);
 
 export function activate(context: ExtensionContext) {
-  restart();
+  function didOpenTextDocument(document: TextDocument): void {
+    // We are only interested in language mode text
+    if (document.languageId !== "grain") {
+      return;
+    }
+
+    const uri = document.uri;
+    // Untitled files go to a default client.
+    if (uri.scheme === "untitled" && !defaultClient) {
+      defaultClient = startClient(undefined);
+      defaultClient.start();
+      return;
+    }
+    let folder = workspace.getWorkspaceFolder(uri);
+    // Files outside a folder can't be handled. This might depend on the language.
+    // Single file languages like JSON might handle files outside the workspace folders.
+    if (!folder) {
+      return;
+    }
+    // If we have nested workspace folders we only start a server on the outer most workspace folder.
+    folder = getOuterMostWorkspaceFolder(folder);
+
+    if (!clients.has(folder.uri.toString())) {
+      const client = startClient(folder.uri);
+      clients.set(folder.uri.toString(), client);
+      // Start the client. This will also launch the server
+      client.start();
+    }
+  }
+
+  workspace.onDidOpenTextDocument(didOpenTextDocument);
+  workspace.textDocuments.forEach(didOpenTextDocument);
+  workspace.onDidChangeWorkspaceFolders((event) => {
+    for (const folder of event.removed) {
+      const client = clients.get(folder.uri.toString());
+      if (client) {
+        clients.delete(folder.uri.toString());
+        client.stop();
+      }
+    }
+  });
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (disposables) {
-    disposables.forEach((item) => item.dispose());
+  const promises: Thenable<void>[] = [];
+  if (defaultClient) {
+    promises.push(defaultClient.stop());
   }
-  disposables = [];
-  if (!client) {
-    return undefined;
+  for (const client of clients.values()) {
+    promises.push(client.stop());
   }
-  return client.stop();
+  return Promise.all(promises).then(() => undefined);
 }
